02f2d3d20a7c5c9b15c83296c628fc73176c6ac6
alexkinsey <alexanderkinsey@icloud.com>
2021-04-13 16:52:40 +0100

    end of lab

---
+++ client/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <sightings-form />
+    <sightings-grid :sightings="sightings" />
+  </div>
+</template>
+
+<script>
+import SightingsForm from './components/SightingsForm';
+import SightingsGrid from './components/SightingsGrid';
+import SightingService from './services/SightingService.js';
+import { eventBus } from '@/main.js';
+
+export default {
+  name: 'app',
+  components: {
+    'sightings-form': SightingsForm,
+    'sightings-grid': SightingsGrid
+  },
+  data() {
+    return {
+      sightings: []
+    };
+  },
+	mounted() {
+    this.fetchSightings();
+    eventBus.$on('sighting-added', (sighting) => {
+      console.log('display bird');
+      this.sightings.push(sighting);
+    });
+  },
+  methods: {
+    fetchSightings() {
+      SightingService.getSightings()
+        .then(sightings => this.sightings = sightings);
+    }
+    
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  background: url('./assets/birds-background.jpg') no-repeat;
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+}
+</style>
